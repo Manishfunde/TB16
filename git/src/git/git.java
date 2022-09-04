9f7820c3e0f4113614f2fa120511dfb9aa2cf4ec
lenovo <lenovo@192.168.0.10>
2022-09-04 12:24:59 +0530

    git practice

---
+++ git/src/git/git.java
@@ -0,0 +1,34 @@
+package git;
+
+public class git 
+{
+	public void m1()
+	{
+		System.out.println("manish");
+		}
+	public void M2()
+	{
+		System.out.println("shruti");
+	}
+	public void m3()
+	{
+		System.out.println("vaibhav");
+	}
+	public void m4()
+	{
+		System.out.println("funde");
+	}
+	public void m5()
+	{
+		System.out.println("avinash");
+	}
+	public static void main(String[] args) 
+	{
+		demo d=new demo();
+		d.m1();
+		d.M2();
+		d.m3();
+		d.m4();
+		d.m5();
+}
+}
